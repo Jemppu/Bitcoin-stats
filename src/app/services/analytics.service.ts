aa755ce0dffb8f6f86bd83276f89d8b5bf5796ae
Jemppu <60470942+Jemppu@users.noreply.github.com>
2021-11-17 11:33:24 +0200

    logic ready

--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -10,26 +10,94 @@ export class AnalyticsService {
   datesToUnix(dates = [new Date]){
     let datesUnix = [];
     for (var date of dates){
-      datesUnix.push(date.getTime()/1000 + 3600);
+      datesUnix.push(date.getTime()/1000 + 7200);
     }
     return datesUnix
   }
 
   getJson(url = 'string'){
-    var Httpreq = new XMLHttpRequest(); // a new request
+    var Httpreq = new XMLHttpRequest();
     Httpreq.open("GET",url,false);
     Httpreq.send(null);
     var json = JSON.parse(Httpreq.responseText);
     return json;          
 }
 
+  longestBearish(prices = []){
+    var longest = 0;
+    var current = 0;
+    var decreased = false;
+    var startTime = 0;
+    var endTime = 0;
+    for (let i = 1; i < prices.length; i++){
+      if (prices[i][1] < prices[i - 1][1]){
+        current += 1;
+        if ( current > longest){
+            decreased = true;
+            longest = current;
+            endTime = prices[i][0];
+            startTime = prices[i-current][0];
+        }
+      }
+      else{
+        current = 0;
+      }
+    }
+    if (decreased){
+      return [new Date(startTime), new Date(endTime)];
+    }
+    else{
+      return [];
+    }
+  }
+
+  highestVolume(volume = []){
+    var highest = 0;
+    var highestTime = 0;
+    for (let i = 0; i < volume.length; i++){
+      if (volume[i][1] > highest){
+        highest = volume[i][1];
+        highestTime = volume[i][0];
+      }
+    }
+    return [new Date(highestTime), highest]
+  }
+
+  maximumProfit(prices = []){
+    var lowestTime = prices[0][0];
+    var lowestPrice = prices[0][1];
+    var differenceStartTime = prices[0][0];
+    var differenceEndTime = prices[0][0];
+    var differenceAmount = 0;
+    for (let i = 1; i < prices.length; i++){
+      if(prices[i][1] - lowestPrice > differenceAmount){
+        differenceAmount = prices[i][1] - lowestPrice;
+        differenceStartTime = lowestTime;
+        differenceEndTime = prices[i][0];
+      }
+      if(prices[i][1] < lowestPrice){
+        lowestPrice = prices[i][1];
+        lowestTime = prices[i][0];
+      }
+    }
+    if(differenceStartTime == differenceEndTime){
+      return [];
+    }
+    return [new Date(differenceStartTime), new Date(differenceEndTime)];
+  } 
 
   analyzeDates(){
-    let dates = [new Date ('2015.05.03'), new Date ('2016.05.03')];
+    let dates = [new Date ('2020.01.01'), new Date ('2021.01.01')];
     let datesUnix = this.datesToUnix(dates);
     let url = 'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=' + datesUnix[0] + '&to=' + datesUnix[1];
     var json = this.getJson(url);
-    console.log(json);
+    let bearishDates = [];
+    bearishDates = this.longestBearish(json.prices);
+    let highVolume = [];
+    highVolume = this.highestVolume(json.total_volumes);
+    let profitDates = [];
+    profitDates = this.maximumProfit(json.prices);
+    console.log(profitDates);
   }
 
   getAnalytics() {
