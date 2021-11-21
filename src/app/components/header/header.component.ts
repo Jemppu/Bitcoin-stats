6e2731cfa42701b594c54880e26bebd113cdc107
Jemppu <60470942+Jemppu@users.noreply.github.com>
2021-11-21 12:08:31 +0200

    html cleaning

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -17,20 +17,29 @@ export class HeaderComponent implements OnInit {
   formattedVolumeTime: String;
   formattedMaxProfitStart: String;
   formattedMaxProfitEnd: String;
+  showText: boolean = false;
+  profitToBeMade: boolean = false;
+  profitNotToBeMade: boolean = false;
+  error: boolean = false;
 
   constructor(private analyticsService: AnalyticsService) {
   }
 
   ngOnInit(): void { }
-
   searchTimePeriod(times: Date[]) {
+    this.showText = false;
     this.analyticsService.analyzeDates(times)
       .then(data => {
+        this.showText = true;
         this.longestBearish = data[0],
-        this.highestVolume = data[1],
-        this.formattedVolumeTime = data[2],
-        this.formattedMaxProfitStart = data[3],
-        this.formattedMaxProfitEnd = data[4]
+          this.highestVolume = data[1],
+          this.formattedVolumeTime = data[2],
+          this.formattedMaxProfitStart = data[3],
+          this.formattedMaxProfitEnd = data[4],
+          this.profitToBeMade = data[3] != "",
+          this.profitNotToBeMade = data[3] == "",
+          this.showText = data[0] != "data not found",
+          this.error = data[0] == "data not found"
       });
 
     this.formattedStartTime = `${times[0].getFullYear()}-${times[0].getMonth() + 1}-${times[0].getDate()}`;
--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -15,16 +15,17 @@ interface MarketChart {
 export class AnalyticsService {
 
   json: MarketChart;
-  longestDecrease: String;
-  highestVolumeString: String;
-  highestVolumeTime: String;
-  profitDateStart: String;
-  profitDateEnd: String;
+  longestDecrease: String = "";
+  highestVolumeString: String = "";
+  highestVolumeTime: String = "";
+  profitDateStart: String = "";
+  profitDateEnd: String = "";
   prices: Array<Array<number>>;
   total_volumes: Array<Array<number>>;
 
   constructor() { }
 
+  //simple method that retuns given array of dates as unix timestamps
   datesToUnix(dates: Array<Date>) {
     let datesUnix = [];
     for (var date of dates) {
@@ -87,12 +88,15 @@ export class AnalyticsService {
       }
     }
     if (differenceStartTime == differenceEndTime) {
-      return;
+      this.profitDateStart = "";
+      this.profitDateEnd = "";
     }
-    let diffStartDate = new Date(differenceStartTime)
-    let diffEndDate = new Date(differenceEndTime)
-    this.profitDateStart = `${diffStartDate.getFullYear()}-${diffStartDate.getMonth() + 1}-${diffStartDate.getDate()}`;
-    this.profitDateEnd = `${diffEndDate.getFullYear()}-${diffEndDate.getMonth() + 1}-${diffEndDate.getDate()}`;
+    else {
+      let diffStartDate = new Date(differenceStartTime)
+      let diffEndDate = new Date(differenceEndTime)
+      this.profitDateStart = `${diffStartDate.getFullYear()}-${diffStartDate.getMonth() + 1}-${diffStartDate.getDate()}`;
+      this.profitDateEnd = `${diffEndDate.getFullYear()}-${diffEndDate.getMonth() + 1}-${diffEndDate.getDate()}`;
+    }
   }
 
   analyzeDates = async (dates: Array<Date>) => {
@@ -100,23 +104,31 @@ export class AnalyticsService {
     await this.getJson(datesUnix)
       .then(response => response.json())
       .then(data => this.json = data)
+      .catch((error) => {
+        console.error('Error:', error)
+      });
     //api gives results for every hour when query is shorter than 90 days, picking data for once per day
-    if (datesUnix[1] - datesUnix[0] < 7776000) {
-      this.prices = Array(this.json.prices[0])
-      this.total_volumes = Array(this.json.total_volumes[0])
-      for (let i = 24; i < this.json.prices.length; i += 24) {
-        this.prices.push(this.json.prices[i])
-        this.total_volumes.push(this.json.total_volumes[i])
+    if (this.json) {
+      if (datesUnix[1] - datesUnix[0] < 7776000) {
+        this.prices = Array(this.json.prices[0])
+        this.total_volumes = Array(this.json.total_volumes[0])
+        for (let i = 24; i < this.json.prices.length; i += 24) {
+          this.prices.push(this.json.prices[i])
+          this.total_volumes.push(this.json.total_volumes[i])
+        }
       }
+      else {
+        this.prices = this.json.prices;
+        this.total_volumes = this.json.total_volumes;
+      }
+      this.longestBearish(this.prices);
+      this.highestVolume(this.total_volumes);
+      this.maximumProfit(this.prices);
     }
-    else {
-      this.prices = this.json.prices;
-      this.total_volumes = this.json.total_volumes;
+    else{ 
+      this.longestDecrease = "data not found"
     }
-    this.longestBearish(this.prices);
-    this.highestVolume(this.total_volumes);
-    this.maximumProfit(this.prices);
-
     return [this.longestDecrease, this.highestVolumeString, this.highestVolumeTime, this.profitDateStart, this.profitDateEnd];
+
   }
 }
